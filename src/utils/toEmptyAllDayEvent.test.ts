927ca2c94ce1bba3b11dc9655e66e9c2d2e92825
Tobiáš Potoček <tobias.potocek@apify.com>
2024-02-24 22:26:12 +0100

    fix: make end date of all-day events exclusive
    This is part of Google Calendar specification. It actually worked
    but some calendar clients might not support it correctly.

---
+++ src/utils/toEmptyAllDayEvent.test.ts
@@ -0,0 +1,66 @@
+import { toEmptyAllDayEvent } from './toEmptyAllDayEvent.js';
+import { EventBuilder } from '../../test/utils/EventBuilder.js';
+
+describe('toEmptyAllDayEvent', () => {
+    it('converts a normal event to an all-day event', () => {
+        const event = toEmptyAllDayEvent(
+            new EventBuilder()
+                .on('2024-02-05')
+                .from('10:00')
+                .to('11:00')
+                .toEvent());
+
+        expect(event.start?.date).toBe('2024-02-05');
+        expect(event.end?.date).toBe('2024-02-06'); // End date is exclusive
+    });
+
+    it('keeps an all-day event as is', () => {
+        const event = toEmptyAllDayEvent(
+            new EventBuilder()
+                .on('2024-02-05')
+                .toEvent());
+
+        expect(event.start?.date).toBe('2024-02-05');
+        expect(event.end?.date).toBe('2024-02-06'); // End date is exclusive
+    });
+
+    it('converts a multi-day event with specific start/end time to a multi-day all-day event', () => {
+        const event = toEmptyAllDayEvent(
+            new EventBuilder()
+                .from('2024-02-05', '16:00')
+                .to('2024-02-07', '02:00')
+                .toEvent());
+
+        expect(event.start?.date).toBe('2024-02-05');
+        expect(event.end?.date).toBe('2024-02-08'); // End date is exclusive
+    });
+
+    it('keeps a multi-day all-day event as is', () => {
+        const event = toEmptyAllDayEvent(
+            new EventBuilder()
+                .from('2024-02-05')
+                .to('2024-02-10')
+                .toEvent());
+
+        expect(event.start?.date).toBe('2024-02-05');
+        expect(event.end?.date).toBe('2024-02-11'); // End date is exclusive
+    });
+
+    it('strips all other information from the event', () => {
+        const event = {
+            summary: 'Test event',
+            start: { dateTime: '2024-02-05T10:00:00.000Z' },
+            end: { dateTime: '2024-02-05T11:00:00.000Z' },
+            description: 'This is a test event',
+            eventType: 'outOfOffice',
+        };
+
+        const emptyAllDayEvent = toEmptyAllDayEvent(event);
+
+        expect(emptyAllDayEvent).toEqual({
+            summary: 'Test event',
+            start: { date: '2024-02-05' },
+            end: { date: '2024-02-06' },
+        });
+    });
+});
--- src/utils/toEmptyAllDayEvent.ts
+++ src/utils/toEmptyAllDayEvent.ts
@@ -1,10 +1,23 @@
 import { calendar_v3 } from 'googleapis';
 import Schema$Event = calendar_v3.Schema$Event;
+import dayjs from 'dayjs';
 
+/** Converts an event to an all-day event and strips all other information. */
 export function toEmptyAllDayEvent({ summary, start, end }: Schema$Event): Schema$Event {
-    return {
-        summary,
-        start: { date: start?.dateTime ? start?.dateTime.split('T')[0] : start?.date },
-        end: { date: end?.dateTime ? end?.dateTime.split('T')[0] : end?.date },
-    };
+    // All-day events are kept as they are
+    if (start?.date && end?.date) {
+        return { summary, start, end };
+    }
+
+    // Normal events are converted to all-day events
+    if (start?.dateTime && end?.dateTime) {
+        return {
+            summary,
+            start: { date: dayjs(start?.dateTime).format('YYYY-MM-DD') },
+            // The end is exclusive.
+            end: { date: dayjs(end?.dateTime).add(1, 'day').format('YYYY-MM-DD') }
+        };
+    }
+
+    throw new Error('Invalid event');
 }
